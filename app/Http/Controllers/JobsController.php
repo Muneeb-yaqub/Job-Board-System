172f54c5dd90fe9e456f0ede0c4c255d31886b1d
DESKTOP-4THTUHP\munee <muneeb2703@gmail.com>
2025-02-20 16:19:28 +0500

    my files

---
+++ app/Http/Controllers/JobsController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Job;
+use App\Models\JobType;
+use Illuminate\Http\Request;
+
+class JobsController extends Controller
+{
+    // This method will show jobs page
+    public function index(Request $request) {
+        $categories = Category::where('status',1)->get();
+        $jobTypes = JobType::where('status',1)->get();
+
+        $jobs = Job::where('status',1);
+
+        // Search using keyword
+        if (!empty($request->keyword)) {
+            $jobs = $jobs->where(function($query) use ($request) {
+                $query->orWhere('title','like','%'.$request->keyword.'%');
+                $query->orWhere('keywords','like','%'.$request->keyword.'%');
+            });
+        }
+
+        // Search using location
+        if(!empty($request->location)) {
+            $jobs = $jobs->where('location',$request->location);
+        }
+
+        // Search using category
+        if(!empty($request->category)) {
+            $jobs = $jobs->where('category_id',$request->category);
+        }
+
+        $jobTypeArray = [];
+        // Search using Job Type
+        if(!empty($request->jobType)) {
+            $jobTypeArray = explode(',',$request->jobType);
+
+            $jobs = $jobs->whereIn('job_type_id',$jobTypeArray);
+        }
+
+        // Search using experience
+        if(!empty($request->experience)) {
+            $jobs = $jobs->where('experience',$request->experience);
+        }
+
+        $jobs = $jobs->with(['jobType','category']);
+
+        if($request->sort == '0') {
+            $jobs = $jobs->orderBy('created_at','ASC');
+        } else {
+            $jobs = $jobs->orderBy('created_at','DESC');
+        }
+        
+
+        $jobs = $jobs->paginate(9);
+
+
+        return view('front.jobs',[
+            'categories' => $categories,
+            'jobTypes' => $jobTypes,
+            'jobs' => $jobs,
+            'jobTypeArray' => $jobTypeArray
+        ]);
+    }
+
+    // This method will show job detail page
+    public function detail($id) {
+
+        $job = Job::where([
+            'id' => $id, 
+            'status' => 1
+        ])->with(['jobType','category'])->first();
+        
+        if ($job==null) {
+            abort(404);
+        }
+        return view('front.jobDetail',['job' => $job ]);
+    }
+}
